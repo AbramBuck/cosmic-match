170a8a9f8601b3d428864ea8511892a533b8a68a
AbramBuck <abram.buck@gmail.com>
2024-11-15 11:37:32 -0600

    shuffle mechanic working

--- react-vite/src/components/MatchGame/MatchGame.jsx
+++ react-vite/src/components/MatchGame/MatchGame.jsx
@@ -1,9 +1,36 @@
 import "../MatchGame/MatchGame.css"
 import confetti from 'https://cdn.skypack.dev/canvas-confetti';
 import matchSnd from '../../audio/CardMatched.mp3'
+import { useState } from "react";
+import cardBack from '../../../src/images/cover.png';
+
+const cardDeck = [
+    { "src" : "../../../src/images/helmet-1.png"},
+    { "src" : "../../../src/images/potion-1.png"},
+    { "src" : "../../../src/images/ring-1.png"},
+    { "src" : "../../../src/images/scroll-1.png"},
+    { "src" : "../../../src/images/shield-1.png"},
+    { "src" : "../../../src/images/sword-1.png"},
+]
+
 
 function MatchGame() {
     let matchedAudio = new Audio(matchSnd);
+
+    const [cards, setCards] = useState([]);
+    const [turns, setTurns] = useState(0);
+
+    //shuffle
+    const shuffleCards = () => {
+        const shuffledCards = [...cardDeck, ...cardDeck]
+            .sort(() => Math.random() -0.5)
+            .map((card) => ({...card, id: Math.random()}))
+        
+        setCards(shuffledCards)
+        setTurns(0)
+    }
+
+    console.log("Cards //////////:", cards, "Turns ////////", turns)
 
     const gotMatch = () => {
         confetti()
@@ -15,6 +42,17 @@ function MatchGame() {
             <h1 className="mg-title">Planet Zarros</h1>
             <div className="game-area">
             <button onClick={gotMatch}>Matched Card</button>
+            <button onClick={shuffleCards}>Start New Game</button>
+            <div className="card-grid">
+                {cards.map(card =>(
+                    <div className="card" key={card.id}>
+                        <div>
+                            <img className="front" src={card.src} alt="card front" />
+                            <img className="back" src="../../../src/images/cover.png" alt="card back"/>
+                        </div>
+                    </div>
+                ))}
+            </div>
             </div>
         </div>    
     )
