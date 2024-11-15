ae58fa036e8d7f9b4cf04e2c2c35e790e56829ce
AbramBuck <abram.buck@gmail.com>
2024-11-15 10:47:45 -0600

    Audio Effects working

--- react-vite/src/components/MatchGame/MatchGame.jsx
+++ react-vite/src/components/MatchGame/MatchGame.jsx
@@ -1,15 +1,18 @@
 import "../MatchGame/MatchGame.css"
 import confetti from 'https://cdn.skypack.dev/canvas-confetti';
+import matchSnd from '../../audio/CardMatched.mp3'
 
 function MatchGame() {
+    let matchedAudio = new Audio(matchSnd);
 
     const gotMatch = () => {
         confetti()
+        matchedAudio.play();
     }
 
     return(
         <div className="page-wrapper">
-            <h1 className="mg-title">Match Game</h1>
+            <h1 className="mg-title">Planet Zarros</h1>
             <div className="game-area">
             <button onClick={gotMatch}>Matched Card</button>
             </div>
