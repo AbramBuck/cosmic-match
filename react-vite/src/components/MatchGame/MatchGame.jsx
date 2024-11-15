fb3dcff7eb383a2ee3a4cc74c1aac6a01d5aa934
AbramBuck <abram.buck@gmail.com>
2024-11-15 10:26:07 -0600

    confetti effect working

--- react-vite/src/components/MatchGame/MatchGame.jsx
+++ react-vite/src/components/MatchGame/MatchGame.jsx
@@ -1,12 +1,17 @@
 import "../MatchGame/MatchGame.css"
+import confetti from 'https://cdn.skypack.dev/canvas-confetti';
 
 function MatchGame() {
+
+    const gotMatch = () => {
+        confetti()
+    }
 
     return(
         <div className="page-wrapper">
             <h1 className="mg-title">Match Game</h1>
             <div className="game-area">
-
+            <button onClick={gotMatch}>Matched Card</button>
             </div>
         </div>    
     )
