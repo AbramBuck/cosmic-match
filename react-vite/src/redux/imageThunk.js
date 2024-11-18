e67446e150a988ceb81c38ffccdf01097b549b94
AbramBuck <abram.buck@gmail.com>
2024-11-17 22:00:16 -0600

    React form and thunk added. Not working.

---
+++ react-vite/src/redux/imageThunk.js
@@ -0,0 +1,13 @@
+export const createImage = (post) => async (dispatch) => {
+    const response = await fetch(`/images/new`, {
+      method: "POST",
+      body: post
+    });
+  
+    if (response.ok) {
+        const { resPost } = await response.json();
+        dispatch(addPost(resPost));
+    } else {
+        console.log("There was an error making your post!")
+    }
+};
