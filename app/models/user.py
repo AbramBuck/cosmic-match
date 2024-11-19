a883590b8249ed83dcfa59feefbe4272b37db67e
AbramBuck <abram.buck@gmail.com>
2024-11-19 11:51:04 -0600

    Updated user table with gold, total runs

--- app/models/user.py
+++ app/models/user.py
@@ -3,6 +3,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 
 
+
 class User(db.Model, UserMixin):
     __tablename__ = 'users'
 
@@ -12,7 +13,12 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(40), nullable=False, unique=True)
     email = db.Column(db.String(255), nullable=False, unique=True)
+    gold = db.Column(db.Integer, default=500)
+    current_ship = db.Column(db.Integer, default=1)
+    total_runs = db.Column(db.Integer, default=0)
     hashed_password = db.Column(db.String(255), nullable=False)
+
+
 
     @property
     def password(self):
@@ -29,5 +35,8 @@ class User(db.Model, UserMixin):
         return {
             'id': self.id,
             'username': self.username,
-            'email': self.email
+            'email': self.email,
+            'gold': self.gold,
+            'current_ship': self.current_ship,
+            'total_runs': self.total_runs,
         }
--- react-vite/src/components/SpaceStationHub/SpaceStationHub.jsx
+++ react-vite/src/components/SpaceStationHub/SpaceStationHub.jsx
@@ -1,12 +1,27 @@
 import { Link } from "react-router-dom";
-
+import { useSelector } from "react-redux";
 
 function SpaceStationHub() {
+    const User = useSelector((state) => state.session.user);
+    console.log("USER INFO:////////", User);
 
+    let level = 1;
+
+    if (User.total_runs < 20) {
+        level = 0;
+    } else {
+        level = User.total_runs / 20
+    }
 
     return (
         <div>
             <h1>Click Below to Launch Mission</h1>
+            <div>
+                <h2>Commander {User.username}</h2>
+                <h2>Level: {level}</h2>
+                <h2>Gold: {User.gold}</h2>
+                <h2>Total Runs: {User.total_runs}</h2>
+            </div>
             <Link to={"/mission"}>LAUNCH MISSION</Link>
         </div>
     )
