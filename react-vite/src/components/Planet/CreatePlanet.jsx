b471d54925b182477adbdffdde5c071fafe8dbed
AbramBuck <abram.buck@gmail.com>
2024-11-23 10:53:05 -0600

    Create a Planet form Styled

--- react-vite/src/components/Planet/CreatePlanet.jsx
+++ react-vite/src/components/Planet/CreatePlanet.jsx
@@ -2,7 +2,7 @@ import  { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { createPlanet } from '../../redux/planet';
 import { useNavigate } from 'react-router-dom';
-
+import './CreatePlanet.css'
 
 
 const CreatePlanet = () => {
@@ -12,6 +12,7 @@ const CreatePlanet = () => {
     const [image, setImageUrl] = useState("https://res.cloudinary.com/di0fa12vz/image/upload/v1732376498/default_planet_fzsx8x.jpg");
     const [deckSize, setDeckSize] = useState(6);
     const [error, setErrors] = useState(null); 
+
 
     const handleChange = async (e) => {
         setDeckSize(Number(e.target.value))
@@ -36,63 +37,64 @@ const CreatePlanet = () => {
 
 
   return (
-    <div className="Create-wrapper">
-    
-    <div className="create-note-container">
-      <h1>Create a New Planet</h1>
-      <form onSubmit={handleSubmit}>
-        <div className="form-group">
-          <label htmlFor="name">Planet Name:</label>
-          <input
-            type="text"
-            id="name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            required
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="url">Image url:</label>
-          <input
-            type="text"
-            id="url"
-            value={image}
-            onChange={(e) => setImageUrl(e.target.value)}
-            required
-          />
-        </div>
-        <div className="priority-container">
-                <label><h4>Deck Size</h4></label>
-                <div className="deck-options">
-
-                  <label className="deck-label">
-                    <input
-                      type="radio"
-                      name="deck"
-                      value="6"
-                      checked={deckSize === 6}
-                      onChange={handleChange}
-                    />
-                    <span className="deck-btn">6</span>
-                  </label>
-
-                  <label className="deck-label">
-                    <input
-                      type="radio"
-                      name="deck"
-                      value="12"
-                      checked={deckSize === 12}
-                      onChange={handleChange}
-                    />
-                    <span className="deck-btn">12</span>
-                  </label>
+    <div className="create-planet-page-wrapper">
+        <div className='frosted-glass create-planet-form-glass'>
+            <div className="create-container">
+            <h1>Create a New Planet</h1>
+            <form onSubmit={handleSubmit}>
+                <div className="form-group">
+                <label htmlFor="name">Planet Name:</label>
+                <input
+                    type="text"
+                    id="name"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    required
+                />
                 </div>
-        </div>
-        <button type="submit">Create Planet</button>
-      </form>
+                <div className="form-group">
+                <label htmlFor="url">Image url:</label>
+                <input
+                    type="text"
+                    id="url"
+                    value={image}
+                    onChange={(e) => setImageUrl(e.target.value)}
+                    required
+                />
+                </div>
+                <div className="priority-container">
+                        <label><h4>Deck Size</h4></label>
+                        <div className="deck-options">
 
-      {error && <p className="error">{error}</p>}
-    </div>
+                        <label className="deck-label">
+                            <input
+                            type="radio"
+                            name="deck"
+                            value="6"
+                            checked={deckSize === 6}
+                            onChange={handleChange}
+                            />
+                            <span className="deck-btn">6</span>
+                        </label>
+
+                        <label className="deck-label">
+                            <input
+                            type="radio"
+                            name="deck"
+                            value="12"
+                            checked={deckSize === 12}
+                            onChange={handleChange}
+                            />
+                            <span className="deck-btn">12</span>
+                        </label>
+                        </div>
+                </div>
+                <button type="submit">Create Planet</button>
+            </form>
+
+            {error && <p className="error">{error}</p>}
+            </div>
+        </div>
     </div>
   );
 };
