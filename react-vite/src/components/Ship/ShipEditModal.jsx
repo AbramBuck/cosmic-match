920185bc86a7e2a70c92c97a54c6864780afd994
AbramBuck <abram.buck@gmail.com>
2024-12-01 14:22:17 -0600

    Edit ships working

--- react-vite/src/components/Ship/ShipEditModal.jsx
+++ react-vite/src/components/Ship/ShipEditModal.jsx
@@ -1,65 +1,51 @@
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useModal } from "../../context/Modal";
-import { updatePlanet, getAllPlanets } from "../../redux/planet";
-import { FaSdCard } from "react-icons/fa";
 import { RiSpaceShipFill } from "react-icons/ri";
 import "../Card/EditModal.css";
 import '../SingleCard/SingleCard.css'
-import imageDefault from "../../../src/images/items/default-card-image.jpg"
-import { fetchShips } from "../../redux/ship";
+import imageDefault from "../../../src/images/planets/none-selected-image-msg.jpg"
+import { thunkShipUpdate, fetchShips } from "../../redux/ship";
+import ShipImages from '../SpaceStationHub/DefaultShipImages';
 
-
-function ShipEditModal({ card }) {
-    const dispatch = useDispatch();
+function ShipEditModal({ ship }) {
+    const dispatch = useDispatch()
     const noneSelected = imageDefault
-    const [name, setName] = useState(card.name)
-    const [imageUrl, setImageUrl] = useState(card.image_url)
-    const [hostileRating, setHostileRating] = useState(card.hostile);
-    const [planetId, setPlanetId] = useState(card.planet_id);
-    const [errors, setErrors] = useState({});
-    const { closeModal } = useModal();
-    const planets = useSelector((state) => state.planets.planets)
-    const cards = useSelector((state) => state.cards.cards )
-    const cardsOnPlanet = cards.reduce((acc, card) => {
-      const id = card.planet_id;
-      acc[id] = (acc[id] || 0) + 1; 
-      return acc;
-    }, {});
-    const thisCardsPlanet = planets.filter((planet) => planet.id === card.planet_id);
-    const cardsAtMax = cardsOnPlanet >= thisCardsPlanet.deck_size
+    const [name, setName] = useState(ship.name)
+    const [imageUrl, setImageUrl] = useState(ship.image_url)
+    const [shipId, setPlanetId] = useState(ship.id)
+    const [errors, setErrors] = useState({})
+    const { closeModal } = useModal()
+    const ships = useSelector((state) => state.ships.ships )
+
+    useEffect(() => { 
+        dispatch(fetchShips())
+    }, [dispatch], ships);
+
+
     const handleSubmit = async (e) => {
-      e.preventDefault();
-      
+        e.preventDefault();
 
-      if (cardsAtMax === true) {
-        alert("This planet's deck is maxed out. Delete some cards on this planet to add more")
-      } else {
-
-        const cardData = {
-          planet_id: planetId,
-          name: name,
-          image_url: imageUrl,
-          hostile: hostileRating,
-        };
+        const shipData = {
+            name: name,
+            image_url: imageUrl,
+          }
 
         try {
-          await dispatch(thunkUpdateCard(card.id, cardData));
-          dispatch(thunkFetchCards()); 
+          await dispatch(thunkShipUpdate(ship.id, shipData))
+          await dispatch(fetchShips())
           closeModal();
         } catch (error) {
-    
-          console.error("Error:", error);
-          setErrors({ general: "An error occurred." });
+          console.error("Error:", error)
+          setErrors({ general: "An error occurred." })
         }
       }
-    };
 
     return (
       <>
       <div className='edit-modal-content'>
         <div className="shell frosted-glass">
-        <div className={ hostileRating && hostileRating === true ? "create-planet-crop-container create-card-border-radius hostile edit-modal-size" : "create-planet-crop-container create-card-border-radius edit-modal-size" }><img src={imageUrl != "" ? imageUrl : noneSelected} title="Add a planet image to preview it"></img></div>
+        <div className={ships ? "create-planet-crop-container create-card-border-radius edit-modal-size" : "create-planet-crop-container create-card-border-radius edit-modal-size" }><img src={imageUrl != "" ? imageUrl : noneSelected} title="Add a ship image to preview it"></img></div>
         <form onSubmit={handleSubmit}>
         <h1><RiSpaceShipFill />Edit Your Ship</h1>
             <div className="form-group">
@@ -74,8 +60,24 @@ function ShipEditModal({ card }) {
                 onChange={(e) => setName(e.target.value)}
                 required
             />
+             <div className="form-group margin-top-20">
+            <label htmlFor="ship images">Image Select</label>
+            <select
+            id="ship_images"
+            value={imageUrl}
+            onChange={(e) => setImageUrl(e.target.value)}
+            title="Select a Ship Image"
+            >
+            <option value="">Select a Ship Image</option>
+            {ShipImages.map((ship) => (
+                <option key={ship.src} value={ship.src}>
+                {ship.name}
+                </option>
+            ))}
+            </select>
+        </div>
              <label htmlFor="image_url" className="form-title">
-                Planet Image Url:
+                Ship Image Url:
             </label>
             {errors.image_url && <p>{errors.image_url}</p>}
             <input
@@ -86,55 +88,10 @@ function ShipEditModal({ card }) {
                 required
             />
             </div>
-            <div className="priority-container">
-                        
-                </div>
-                <div className="priority-container">
-                <h2>Hostile</h2>
-                <label><h3>Hostile cards damage player shields if they are not matched</h3></label>
-                <div className="card-options">
-
-                <label className="card-label">
-                    <input
-                    type="radio"
-                    name="enemy"
-                    value={true}
-                    checked={hostileRating === true}
-                    onChange={() => setHostileRating(true)}
-                    />
-                    <span className="card-btn">Hostile</span>
-                </label>
-
-                <label className="card-label">
-                    <input
-                    type="radio"
-                    name="enemy"
-                    value={false}
-                    checked={hostileRating === false}
-                    onChange={() => setHostileRating(false)}
-                    />
-                    <span className="card-btn">Non-Hostile</span>
-                </label>
-                </div>
-                        <div className="form-group">
-                            <label htmlFor="card_id">Planet:</label>
-                                <select
-                                id="card_id"
-                                value={planetId}
-                                onChange={(e) => setPlanetId(e.target.value)}
-                                required
-                                >
-                                <option value="">Select a Planet</option>
-                                {planets.map((planet) => (
-                                    <option key={planet.id} value={planet.id}>
-                                    {planet.name} <FaSdCard />: { cardsOnPlanet[planet.id] ? cardsOnPlanet[planet.id] : 0 } | {planets ? planet.deck_size : ""}
-                                    </option>
-                                ))}
-                                </select>
-                        </div>
-                </div>
+          
+                
             <div className="modal-buttons">
-              <button type="submit" onClick={handleSubmit}>Update Card</button>
+              <button type="submit" onClick={handleSubmit}>Update Ship</button>
               <button type="submit" onClick={() => closeModal()}>Cancel</button>
             </div>
 
--- react-vite/src/components/Ship/ViewAllShips.jsx
+++ react-vite/src/components/Ship/ViewAllShips.jsx
@@ -3,8 +3,6 @@ import { useDispatch, useSelector } from "react-redux";
 import { Link, useNavigate, useParams } from 'react-router-dom';
 import CreateShipForm from '../SpaceStationHub/CreateShipForm';
 import { fetchShips } from "../../redux/ship";
-import { MdSdCard } from "react-icons/md";
-import { IoPlanet } from "react-icons/io5";
 import OpenModalButton from "../OpenModalButton/OpenModalButton";
 import ShipEditModal from './ShipEditModal' 
 import ShipDeleteConfirmModal from "./ShipDeleteConfirmModal";
@@ -19,13 +17,11 @@ function ViewAllShips() {
   const navigate = useNavigate()
   const [error, setErrors] = useState(null)
   const [shipLevelById, setShipLevelById] = useState(null)
-  const { planetId } = useParams();
   const user = useSelector((store) => store.session.user)
-  const planets = useSelector((state) => state.planets.planets)
-  const cards = useSelector((state) => state.cards.cards)
   const ships = useSelector((state) => state.ships.ships)
   const User = useSelector((state) => state.session.user);
 
+  console.log ("SHIPS",ships)
 
   if (!user) navigate(`/login`)
 
@@ -34,7 +30,7 @@ function ViewAllShips() {
     }, [dispatch], ships);
 
     useEffect(() => {
-        if (ships) {
+        if (ships && ships.length) {
             const shipRunsById = ships.reduce((acc, ship) => {
                 acc[ship.id] = { runsCompleted: ship.runs_completed };
                 return acc;
@@ -69,32 +65,30 @@ function ViewAllShips() {
       <div className="cards-content-area">
       <h1 className="manage-cards-header"><RiSpaceShipFill /> Ships</h1>
       <div className="ship-page-button-div">
-            <div className={ships ? "create-ship-button" : "create-ship-button pulse-btn"}><OpenModalButton buttonText="Create A Ship"  modalComponent={<CreateShipForm User={User} />}/></div>
+            <div className={"create-ship-button"}><OpenModalButton buttonText="Create A Ship"  modalComponent={<CreateShipForm User={User} />}/></div>
       </div> 
 
         <div className="cards-area">
-              {ships.map((ship) => (
+              {ships && shipLevelById && ships.length ? ships.map((ship) => (
                 <div className="card-instance" key={ship.id}>
                   <div>
-                  <div className="crop-container"><img className="crop-container" src={ship.image_url}></img></div>
+                  <div className="crop-container"><img className="crop-container" src={ships ? ship.image_url : ""}></img></div>
                   </div>
                   <div className="card-title ubuntu-regular"><RiSpaceShipFill />{ship.name}</div>
                   <div className="card-stats">
                     {ships && shipLevelById !== null && shipLevelById[ship.id]?.level !== undefined ? <h4>Lvl: {shipLevelById[ship.id].level}  </h4> : " "}
-                    <h4>Runs Completed: {ship.runs_completed}</h4>
+                    <h4>Runs Completed: {ships ?  ship.runs_completed : " "}</h4>
                   </div>
                   <div className="edit-delete-btn-area">
                   <OpenModalButton buttonText="Edit"  modalComponent={<ShipEditModal ship={ship}/>}/>
                   <OpenModalButton buttonText="Delete"  modalComponent={<ShipDeleteConfirmModal shipId={ship.id}/>}/>
-                    {/* {cardsOncard[card.id] ? <button onClick={alertDelete}>Delete</button> : <OpenModalButton buttonText="Delete"  modalComponent={<DeletecardConfirmModal cardId={card.id}/>}/>} */}
                   </div>      
                   <div className="manage-card-title">
                     
-                    {/* <h3>Cards:{ cardsIncard[card.id] ? cardsIncard[card.id] : 0 } </h3> */}
                   </div>
                 
                 </div>
-              ))}
+              )) : " "}
               </div>
         </div>
     </div>
